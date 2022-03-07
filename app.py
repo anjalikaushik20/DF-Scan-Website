b3520137b4198b0d33b7bafcefe8f6a8aeb9169f
Anjali Kaushik <anjali.kaushik.209@gmail.com>
2022-03-07 19:58:08 +0530

    Update app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from crypt import methods
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/scan", methods=['POST'])
+def scan():
+    print("Scanned")
+
+
+if __name__=="main":
+    app.run(debug=True)
